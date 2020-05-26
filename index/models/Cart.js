1745796b2c3f3e0108387acca5bff6d551ec9a54
lancercd <1311282756@qq.com>
2020-05-26 09:39:10 +0800

    准备提交到github

--- index/models/Cart.js
+++ index/models/Cart.js
@@ -1,5 +1,6 @@
 import ListSelect from './../../utils/ListSelect.js';
 import EventAgent from '../../utils/EventAgent.js';
+import request from '../../utils/request.js';
 
 export default class Cart{
     constructor(el){
@@ -8,32 +9,28 @@ export default class Cart{
         this.Select = new ListSelect(this.el, {
             all: 'select-all',
             one: 'select-one',
-            onChange: function(){
-                this.render();
-            }.bind(this)
+            onChange: function(){ this.render() }.bind(this)
         });
 
         this.oItemsNum = el.getElementsByClassName('number');
-
+        // this.delBtns = el.getElementsByClassName('J_del_item');
 
         this.oSelectNum = el.getElementsByClassName('J_select_num')[0];
         this.oTotalMoney = el.getElementsByClassName('J_total_money')[0];
-        // console.log(this.oSelectNum);
+
+        this.list = el.getElementsByClassName('gwcxd')[0];
         this.init();
     }
 
     init(){
         this.bindEvent();
-        // this.render();
     }
 
     bindEvent(){
+        this.list.addEventListener('click', this.onDelBtnClick.bind(this), false);
         for(let item of this.oItemsNum){
             item.addEventListener('blur', this.onNumBlur.bind(this), false);
         }
-        // for(let item of this.Items){
-        //     item.addEventListener('blur', this.onNumBlur.bind(this), false);
-        // }
     }
 
     onNumBlur(ev){
@@ -48,9 +45,8 @@ export default class Cart{
         const price = Number(item.getElementsByClassName('price')[0].innerText).toFixed(2),
               oSubtotal = item.getElementsByClassName('subtotal')[0];
         oSubtotal.innerText = (Number(num) * price).toFixed(2);
+        this.renderTotalMoney();
     }
-
-    //心情有点不好   害
 
     render(){
         this.renderSelectNum();
@@ -68,19 +64,27 @@ export default class Cart{
         //性能太差老   后面再改老
         // 遍历全部商品一遍  看看是否勾选  勾选了  的话查数量 * 单价 相加
         for(let item of this.Items){
-            // item.addEventListener('blur', this.onNumBlur.bind(this), false);
-            // console.log(selected.has(item.dataset.id));
             if(selected.has(item.dataset.id)){
                 let price = Number(item.getElementsByClassName('price')[0].innerText),
                     num = Number(item.getElementsByClassName('number')[0].value);
-                    // console.log(totalPrice + ( num * price ).toFixed(2));
-                totalPrice += Number(( num * price ));
+                totalPrice += ( num * price );
             }
         }
-        // console.log(totalPrice);
         this.oTotalMoney.innerText = totalPrice.toFixed(2);
+    }
+    onDelBtnClick(ev){
+        const e = new EventAgent(ev);
+        if(e.getTagName() === 'button'){
+            // console.log(e.getTar());
+            // 若为选中去除set中相应值
+            e.getTar().parentNode.parentNode.style.display = 'none';
+            this.render();
+        }
 
+        //发送请求
+        request({
 
+        });
     }
 
 
