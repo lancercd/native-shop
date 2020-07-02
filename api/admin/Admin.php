245b7158d0baf408d0a91030aff38a1a14aa53cb
lancercd <1311282756@qq.com>
2020-07-02 23:23:05 +0800

    后台管理页面的 图表

--- api/admin/Admin.php
+++ api/admin/Admin.php
@@ -6,9 +6,21 @@ use \server\JsonService as Json;
 class Admin extends Base{
     //管理员界面的  矩形图
     public function get_chart_data(){
-        return Json::success([
-            5,6,1,3,8,4,9,1,2,5
-        ]);
+        $beginToday=mktime(0,0,0,date('m'),date('d'),date('Y'));
+        $endToday = strtotime(date('Y-m-d'.'00:00:00',time()+3600*24));
+        $now = time();
+        $one_day = 60*60*24;
+        $data = [];
+
+        $thisTime = $one_day*9;
+        $order_count = $this->table('order')->where("`add_time` > {$beginToday}-{$thisTime} and `add_time` < {$endToday}-{$thisTime}")->field('COUNT(*) num')->find(true);
+        var_dump($order_count);die;
+        $data[] = $order_count['num'];
+        $order_count = $this->table('order')->where("`add_time` > {$beginToday} and `add_time` < {$endToday}")->field('COUNT(*) num')->find();
+        $data[] = $order_count['num'];
+        // return Json::success([
+        //     5,6,1,3,8,4,9,1,2,5
+        // ]);
     }
 
 
@@ -20,19 +32,19 @@ class Admin extends Base{
         $beginToday=mktime(0,0,0,date('m'),date('d'),date('Y'));
         $data = ['num' => [], 'stick' => []];
         // 10天订单量
-        $ten_order_count = $this->table('order')->where("`add_time` > {$ten_day_ago}")->field('count(*) num')->find();
+        $ten_order_count = $this->table('order')->where("`add_time` > {$ten_day_ago}")->field('COUNT(*) num')->find();
         $data['num'][] = $ten_order_count['num'];
         // 总订单量
-        $all_order_count = $this->table('order')->field('count(*) num')->find();
+        $all_order_count = $this->table('order')->field('COUNT(*) num')->find();
         $data['num'][] = $all_order_count['num'];
         // 今日活跃用户
-        $active_user_count = $this->table('user')->field('count(*) num')->where("`last_time` > {$beginToday}")->find();
+        $active_user_count = $this->table('user')->field('COUNT(*) num')->where("`last_time` > {$beginToday}")->find();
         $data['num'][] = $active_user_count['num'];
         // 今日新增用户
-        $today_add_user_count = $this->table('user')->field('count(*) num')->where("`first_time` > {$beginToday}")->find();
+        $today_add_user_count = $this->table('user')->field('COUNT(*) num')->where("`first_time` > {$beginToday}")->find();
         $data['num'][] = $today_add_user_count['num'];
         // 总用户数
-        $user_count = $this->table('user')->field('count(*) num')->find();
+        $user_count = $this->table('user')->field('COUNT(*) num')->find();
         $data['num'][] = $user_count['num'];
 
         $data['stick'][] = sprintf("%.1f", $ten_order_count['num']/$all_order_count['num']);
