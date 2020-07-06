f13244fb64de39dfe884d7c2bdbcd493b31f0c10
lancercd <1311282756@qq.com>
2020-07-06 23:03:42 +0800

    后台管理页面 侧边栏菜单 独立成文件

--- admin/sys.php
+++ admin/sys.php
@@ -33,73 +33,7 @@ $order_nonfifish = $admin->get_op_order_count();
 <body>
 <div class="container">
     <!-- 侧边栏 -->
-    <div class="sidebar-container">
-        <div class="userinfo">
-            <h3>商城管理</h3>
-            <div class="head-photo">
-                <img src="<?php echo $avatar ?>" alt="">
-            </div>
-            <div class="nickname">刘灿</div>
-        </div>
-        <!-- 菜单 -->
-        <div class="menu">
-
-
-
-            <li class="item" id="index">
-                <a href="sys.php" class="btn">
-                    <icon class="iconfont icon-icon-test8"></icon>首页
-                </a>
-            </li>
-
-
-            <li class="item" id="post-admin">
-                <a href="#post-admin" class="btn">
-                    <icon class="iconfont icon-tieziguanli"></icon><i></i>订单管理
-                </a>
-            <div class="child-menu">
-                    <a href="order.php">所有订单</a>
-                    <!-- <a href="post_check.php?stype=witPbu">待审核</a>
-                    <a href="post_check.php?stype=noPbu">已完成</a> -->
-            </div>
-            </li>
-
-
-            <li class="item" id="comments">
-                <a href="#comments" class="btn">
-                    <icon class="iconfont icon-icon-test1">
-                    </icon>评论<i></i>
-                </a>
-                <div class="child-menu">
-                    <a href="">评论管理</a>
-                </div>
-            </li>
-
-
-            <li class="item" id="user_admin">
-                <a href="#user_admin" class="btn">
-                    <icon class="iconfont icon-icon-test12">
-                    </icon>用户管理<i></i></a>
-                    <div class="child-menu">
-                    <a href="users.php">用户</a>
-                    </div>
-            </li>
-
-
-            <li class="item" id="self-info">
-                <a href="#self-info" class="btn">
-                    <icon class="iconfont icon-icon-test4">
-                    </icon>个人信息<i></i>
-                </a>
-                <div class="child-menu">
-                    <a href="self_info.php">修改</a>
-                </div>
-            </li>
-
-
-        </div>
-        <!-- /菜单 -->
-    </div>
+    <?php require_once('./components/side_bar.php'); ?>
     <!-- /侧边栏 -->
 
 
